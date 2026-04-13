8c9d8474e9915c5e38e5b046313f30a06f6c85aa
Akash-Karambalkar <karambalkar.ak@gmail.com>
2026-04-13 12:36:42 +0530

    api key sequerd

--- react-ai-tool/src/constants.js
+++ react-ai-tool/src/constants.js
@@ -1,21 +1,3 @@
 const apiKey = import.meta.env.VITE_API_KEY;
-console.log(apiKey);
-console.log(import.meta.env.VITE_API_KEY);
 
 export const URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-3.1-flash-lite-preview:generateContent?key=${apiKey}`;
-
-// curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent" \
-//   -H 'Content-Type: application/json' \
-//   -H 'X-goog-api-key: AIzaSyBfBUXdnk0zxnygtC18X05iijMb3ZmKeDg' \
-//   -X POST \
-//   -d '{
-//     "contents": [
-//       {
-//         "parts": [
-//           {
-//             "text": "Explain how AI works in a few words" AIzaSyAqG_ZDd-3Hhj5gwP-Vmw1dfMDMh6k7hDA
-//           }
-//         ]
-//       }
-//     ]
-//   }'
